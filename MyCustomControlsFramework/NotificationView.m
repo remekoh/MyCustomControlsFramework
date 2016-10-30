ad554b0d6e841dafec9edc5bd968cf1982d50a3a
rem{e}koh <git@remekoh.me>
2016-10-30 08:12:50 -0500

    completed basic mode of Frameworks and CocoaPods assignment

---
+++ MyCustomControlsFramework/NotificationView.m
@@ -0,0 +1,21 @@
+//
+//  NotificationView.m
+//  NotificationCustomView
+//
+//  Created by rem{e}koh on 10/10/16.
+//  Copyright © 2016 rem{e}koh. All rights reserved.
+//
+
+#import "NotificationView.h"
+
+@implementation NotificationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
